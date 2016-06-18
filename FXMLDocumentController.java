b87bade415dcdccc2bf56a7bbf162b41146e7a3d
rordercart <kumar.roushan@gmail.com>
2016-06-18 19:48:18 -0400

    Add files via upload

---
+++ FXMLDocumentController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarunyai;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Dhairya
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+     Parent main_ui= FXMLLoader.load(getClass().getResource("TarunyaiUI.fxml"));
+     Scene main_scene = new Scene(main_ui);
+     Stage main_stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+     main_stage.setScene(main_scene);
+     main_stage.show();
+     }
+    @FXML
+    private void exitApplication(ActionEvent event) {
+        System.exit(1);
+        //label.setText("Hello World!");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
